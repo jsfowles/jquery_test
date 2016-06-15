e82a33a3fe8bade49eddcbfb5c54cffeb19af7fa
Jacob Fowles <js.fowles@gmail.com>
2016-06-15 10:35:57 -0600

    jquery yield

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,2 +1,9 @@
-// all of our custom js code
-// dealing with the dashboard views
+// wait for the dom to be ready
+// IMPORTANT = ADD THIS TO EACH JS FILE YOU WRITE!!!
+$(document).on('ready', function() {
+
+  function sayHello() {
+    alert('button clicked, Hello!');
+  }
+
+});
