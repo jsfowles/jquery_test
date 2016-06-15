ae8598a64ac85c0b6231c1ed6caae1fb0e848a1f
Jacob Fowles <js.fowles@gmail.com>
2016-06-15 11:22:15 -0600

    userinputs

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,32 @@
 // wait for the dom to be ready
 // IMPORTANT = ADD THIS TO EACH JS FILE YOU WRITE!!!
 $(document).on('ready', function() {
+  // vanilla JS
+  // var helloButton = document.getElementById('hello-button');
+  // helloButton.addEventListener('click', function() {
+  //   //write any code in here that ppens onclick
+  // });
 
-  function sayHello() {
-    alert('button clicked, Hello!');
-  }
+  // jQuery - or, the right way
+  $('#hello-button').click(function() {
+    $(this).toggleClass('btn');
+  });
 
+  // listen for the click event on the submit button
+  $('#add-user').click(function() {
+    var userNameInput = $('#user-input');
+    var usersList = $('#users-list');
+    usersList.append('<li>' + userNameInput.val() + '</li>');
+
+    //clear input value
+    userNameInput.val(null);
+
+    //focus the users input
+    userNameInput.focus();
+
+  });
+    // keyup
+    $('#user-input').keyup(function(){
+      console.log('user input keydown');
+    });
 });
